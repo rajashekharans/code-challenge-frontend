6c7fe964297851e647331cd14b14e2624ee92666
Raj Soundrarajan <reachtoraj@outlook.com>
2020-07-04 21:58:40 +1000

    Implement root component with sidebar and mapdisplay component

---
+++ src/components/SideBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <a-layout-sider
+    breakpoint="lg"
+    collapsed-width="0"
+    :style="{ minWidth: '300px', height: '100vh'}"
+  >
+    <a-menu theme="dark" mode="inline">
+      <a-menu-item id="select-property-drop-down">
+        <span class="nav-text">Select property from drop down</span>
+      </a-menu-item>
+      <a-select
+        placeholder="Select the property"
+        style="width: 250px"
+        @change="(property) => setProperty({ property })"
+      >
+        <a-select-option v-for="property in properties" :key="property.id">
+          {{ property.title }}
+        </a-select-option>
+      </a-select>
+    </a-menu>
+  </a-layout-sider>
+</template>
+
+<script>
+
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'SideBar',
+  computed: {
+    ...mapGetters('geoJsonSource', {
+      properties: 'getAllProperties',
+    }),
+  },
+  methods: {
+    ...mapActions('geoJsonSource', [
+      'setProperty',
+    ]),
+  },
+};
+</script>
+<style scoped>
+  #select-property-drop-down {
+    margin-top: 40px;
+  }
+</style>
